bf43c57c33f4f483d51d819c4c1c19bbf098a632
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-05-09 20:49:51 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,35 @@
 def reformat_languages(languages)
-  # your code here
+#new hash 
+#add wanted components of orig hash to new hash 
+#add the last style key value pair 
+#return new hash 
+
+oo_hash = {}
+functional_hash={}
+merged_hash={}
+
+oo = languages[:oo]
+functional = languages[:functional]
+
+oo.each do |lang, type|
+  oo_hash[lang] = type
+end 
+
+oo_hash.each do |lang, type|
+  type[:style] = [:oo]
+end 
+
+functional.each do |lang, type|
+  functional_hash[lang] = type
+end 
+
+functional_hash.each do |lang, type|
+  type[:style] = [:functional]
+end 
+
+merged_hash = oo_hash.merge(functional_hash)
+
+merged_hash[:javascript] = {type: "interpreted", style: [:oo,:functional]}
+
+merged_hash
 end
